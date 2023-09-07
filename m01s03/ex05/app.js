4547d1ea83893baed3c9a8ea0b494fe79635ff54
Vasile Catalin <vasile3.catalin@gmail.com>
2023-09-07 22:01:21 +0300

    clasa 07092023
    clasa 07092023

---
+++ m01s03/ex05/app.js
@@ -0,0 +1,59 @@
+var person = {
+  name: 'Dragos',
+  surname: 'Iordache',
+  age: 36,
+  petOwner: false,
+  skills: ['html', 'javascript', 'css'],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
+
+console.warn(`
+  Ma numesc xxx yyy si stiu html si css.
+`);
+console.log(
+  'Ma numesc ' +
+    person.name +
+    ' ' +
+    person.surname +
+    ' si stiu ' +
+    person.skills[0] +
+    ' si ' +
+    person.skills[2] +
+    '.',
+);
+
+console.warn(`
+Sunt xxx si acum invat JavaScript.
+`);
+console.log('Sunt ' + person.name + ' si acum invat JavaScript.');
+
+console.warn(`
+Am x prieteni:larry, ...
+`);
+console.log(
+  'Am ' +
+    person.friends.length +
+    ' prieteni: ' +
+    person.friends[0].name +
+    ', ' +
+    person.friends[1].name +
+    ' si ' +
+    person.friends[2].name +
+    '.',
+);
