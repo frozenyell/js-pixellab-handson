c3a375098a350978985a84a40d3fc1728d75df3f
Vasile Catalin <vasile3.catalin@gmail.com>
2023-09-12 19:15:13 +0300

    Tema 5 sept -p1
    Tema 5 sept -p1

---
+++ m01s02/ex05/app.js
@@ -0,0 +1,10 @@
+// exercitiu suplimentar START
+console.log(27 + 19 >= 30 + 16);
+console.log(2 * 9) < 5 * 8;
+console.log(257 - 134) > 16 * 9;
+console.log(657 - 802) < 39 - 970;
+console.log(85 * 4) === 407 - 67;
+console.log950 < 1600 - 900;
+// exercitiu suplimentar END
+
+console.log('Imi fac tema la JavaScript.');
---
+++ m01s03/ex01/app.js
@@ -0,0 +1,45 @@
+var object = {
+  name: 'Diriglont',
+  surname: 'Ionica',
+  age: 23,
+  petOwner: false,
+  carOwner: true,
+};
+
+console.warn(
+  `Afiseaza fraza “Ma numesc xxx yyy si am xx ani.” in consola, folosind operatorul de concatenare.`,
+);
+
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' ' +
+    'si am' +
+    ' ' +
+    object.age +
+    ' ' +
+    'ani.',
+);
+
+console.warn(
+  'Afiseaza anul in care te-ai nascut scazand varsta pe care ai trecut-o in obiect din anul curent (un proces numit hardcodare).',
+);
+console.log((2023 - object.age).toString());
+
+console.warn(
+  'Adauga o cheie noua de tip boolean cu numele petOwner (observa capitalizarea)',
+);
+
+console.warn(
+  `Afiseaza fraza “xxx yyy are xx ani.” in consola folosind operatorul de concatenare.`,
+);
+console.log(
+  object.name + ' ' + object.surname + ' are ' + object.age + ' ani.',
+);
+
+console.warn(
+  `Afiseaza fraza “xxx s-a nascut in aaaa.” Foloseste tot anul curent pentru a scadea varsta.`,
+);
+console.log(object.name + ' s-a nascut in ' + (2023 - object.age) + '.');
